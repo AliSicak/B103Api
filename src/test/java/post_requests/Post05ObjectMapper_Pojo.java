443442b0d36e2a9167609b7523b345d642d7259c
Ali Sıcak <alisicak@gmail.com>
2023-03-21 23:54:59 +0300

    21.03.2023

--- src/test/java/post_requests/Post05ObjectMapper_Pojo.java
+++ src/test/java/post_requests/Post05ObjectMapper_Pojo.java
@@ -5,17 +5,16 @@ import base_urls.JsonPlaceHolderBaseUrl;
 import io.restassured.response.Response;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Test;
-import test_data.JsonPlaceHolderTestData;
+import pojos.JsonPlaceHolderPojo;
+import util.ObjectMapperUtils;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 
-public class Post05ObjectMapper_Map extends JsonPlaceHolderBaseUrl {
-    /*
+public class Post05ObjectMapper_Pojo extends JsonPlaceHolderBaseUrl {
+     /*
          Given
            1) https://jsonplaceholder.typicode.com/todos
            2) {
@@ -23,11 +22,13 @@ public class Post05ObjectMapper_Map extends JsonPlaceHolderBaseUrl {
                  "title": "Tidy your room",
                  "completed": false
                }
+
+
             I send POST Request to the Url
         Then
             Status code is 201
         And
-            response body is like {
+            response body is like  {
                                     "userId": 55,
                                     "title": "Tidy your room",
                                     "completed": false,
@@ -36,25 +37,25 @@ public class Post05ObjectMapper_Map extends JsonPlaceHolderBaseUrl {
      */
 
     @Test
-    public void post05ObjectMapper() throws IOException {
+    public void post05() {
         //Set the URL
         spec.pathParam("first", "todos");
 
         //Set the expected data
-        Map<String, Object> expectedData = new JsonPlaceHolderTestData().expectedDataMethod(55, "Tidy your room", false);
-        System.out.println("expectedData = " + expectedData);
+        JsonPlaceHolderPojo expectedData = new JsonPlaceHolderPojo(55, "Tidy your room", false);
+
         //Send the request and get the response
-        Response response = given().spec(spec).body(expectedData).post("{first}");
+        Response response = given().spec(spec).when().body(expectedData).post("{first}");
         response.prettyPrint();
 
         //Do Assertion
-        Map<String, Object> actualdData = new ObjectMapper().readValue(response.asString(), HashMap.class);
-        System.out.println("actualdData = " + actualdData);
+        JsonPlaceHolderPojo actualData = ObjectMapperUtils.convertJsonToJava(response.asString(), JsonPlaceHolderPojo.class);
+        System.out.println("actualData = " + actualData);
 
-        assertEquals(201, response.getStatusCode());
-        assertEquals(expectedData.get("userId"), actualdData.get("userId"));
-        assertEquals(expectedData.get("title"), actualdData.get("title"));
-        assertEquals(expectedData.get("completed"), actualdData.get("completed"));
+        assertEquals(201, response.statusCode());
+        assertEquals(expectedData.getUserId(), actualData.getUserId());
+        assertEquals(expectedData.getTitle(), actualData.getTitle());
+        assertEquals(expectedData.getCompleted(), actualData.getCompleted());
 
     }
 }
