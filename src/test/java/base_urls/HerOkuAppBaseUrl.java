e02f22b9328af33fe72eab80ee44133d7e88aed4
Ali Sıcak <alisicak@gmail.com>
2023-03-22 23:33:06 +0300

    22.03.03

--- src/test/java/base_urls/HerOkuAppBaseUrl.java
+++ src/test/java/base_urls/HerOkuAppBaseUrl.java
@@ -1,10 +1,11 @@
 package base_urls;
 
-
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
+
+import static util.AuthenticationHerOkuApp.generateToken;
 
 public class HerOkuAppBaseUrl {
 
@@ -12,6 +13,7 @@ public class HerOkuAppBaseUrl {
 
     @Before//Her test methodundan önce çalışır.
     public void setUp() {
-        spec = new RequestSpecBuilder().setContentType(ContentType.JSON).setBaseUri("https://restful-booker.herokuapp.com").build();
+        spec = new RequestSpecBuilder().addHeader("Cookie", "token=" + generateToken()).
+                setContentType(ContentType.JSON).setBaseUri("https://restful-booker.herokuapp.com").build();
     }
 }
